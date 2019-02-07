ac7947e2c0e496f912f923ec0da1008610d63050
Kellan Arick <kellan5015@gmail.com>
2019-02-07 15:55:28 -0500

    Initial Commit

---
+++ KellanUtilityLibrary/Strings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace KellanUtilityLibrary
+{
+    public class Strings
+    {
+        public static bool ValidPhoneNumber(string phoneNumber)
+        {
+            Regex regex = new Regex(@"^(\d{3}-)?\d{3}-\d{4}");
+            var match = regex.IsMatch(phoneNumber);
+            return match;
+        }
+
+        public static bool ValidZipCode(string zipCode)
+        {
+            Regex regex = new Regex(@"^(\d{5})(-)?(\d{4})?");
+            var match = regex.IsMatch(zipCode);
+            return match;
+        }
+
+        public static bool EqualIgnoreCase(string strA, string strB)
+        {
+            var result = string.Compare(strA.ToUpper(), strB.ToUpper());
+            bool returnResult;
+            if (result == 0)
+            {
+                returnResult = true;
+            }
+            else
+            {
+                returnResult = false;
+            }
+            return returnResult;
+        }
+    }
+}
---
+++ TestKellanUtilityLibrary/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KellanUtilityLibrary;
+
+namespace TestKellanUtilityLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] zips = { "12345", "123456789", "12345-6789" };
+            foreach(var zip in zips)
+            {
+                Console.WriteLine($"{zip} is " + (Strings.ValidZipCode(zip) ? "" : " not ") + "valid");
+            }
+            
+            var phone1 = "322-8888";
+            var phone1valid = Strings.ValidPhoneNumber(phone1);
+            if (phone1valid)
+            {
+                Console.WriteLine($"{phone1} is valid.");
+            }
+            else
+            {
+                Console.WriteLine($"{phone1} is NOT valid.");
+            }
+
+            var phone2 = "513-322-8888";
+            var phone2valid = Strings.ValidPhoneNumber(phone2);
+            if (phone2valid)
+            {
+                Console.WriteLine($"{phone2} is valid.");
+            }
+            else
+            {
+                Console.WriteLine($"{phone2} is NOT valid.");
+            }
+
+            var phone3 = "513-322-888";
+            var phone3valid = Strings.ValidPhoneNumber(phone3);
+            if (phone3valid)
+            {
+                Console.WriteLine($"{phone3} is valid.");
+            }
+            else
+            {
+                Console.WriteLine($"{phone3} is NOT valid.");
+            }
+
+            var zip1 = "45459";
+            var zip1Valid = Strings.ValidZipCode(zip1);
+            if (zip1Valid)
+            {
+                Console.WriteLine($"{zip1} is valid.");
+            }
+            else
+            {
+                Console.WriteLine($"{zip1} is NOT valid.");
+            }
+
+            var zip2 = "454591234";
+            var zip2Valid = Strings.ValidZipCode(zip2);
+            if (zip2Valid)
+            {
+                Console.WriteLine($"{zip2} is valid.");
+            }
+            else
+            {
+                Console.WriteLine($"{zip2} is NOT valid.");
+            }
+
+            var zip3 = "45459-1234";
+            var zip3Valid = Strings.ValidZipCode(zip3);
+            if (zip3Valid)
+            {
+                Console.WriteLine($"{zip3} is valid.");
+            }
+            else
+            {
+                Console.WriteLine($"{zip3} is NOT valid.");
+            }
+
+            var result = Strings.EqualIgnoreCase("ABC", "abc");
+            if(result == true)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("NOT Equal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
